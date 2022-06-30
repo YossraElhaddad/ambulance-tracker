c84e77bca4db7aa4f2b90a14fc6979fa5cba2c18
YossraElhaddad <nspecial56@gmail.com>
2022-06-30 13:44:46 +0200

    Remove unnecessary code and  comments

--- tests/data-fetching.test.js
+++ tests/data-fetching.test.js
@@ -1,33 +1,17 @@
-const db = require('../models/db');
-//const User = require('../models/user');
-const router = require('../routes/user');
 const request = require('supertest');
-const express = require('express');
-const mongoose = require('mongoose');
-Admin = mongoose.mongo.Admin;
 require('dotenv').config();
 
-//jest.mock('node-fetch', ()=>jest.fn())
-const app = new express();
-app.use('/', router);
-
-//let server;
 
 const baseUrl = 'https://ambulance-tracker.herokuapp.com';
 
 beforeAll( async ()=>{
     await process.nextTick(() => {})
-    db.connect();
     global.agent = request(baseUrl);
-});
-
-afterAll(async ()=> {
-    await db.closeConnection();
 });
 
 
 describe('Check if the required data is available', ()=>{
-    
+
     test('check if data is fetched', async () => {
       
         const res = await global.agent.get('/hospitals');
